5568f699ccc412c726fd16b494ce81cc8e9aaddf
Serge Hänni <serge.haenni@simplificator.com>
2018-10-04 15:57:14 +0200

    Minor improvements.

--- bot.rb
+++ bot.rb
@@ -23,6 +23,7 @@ def add_votee(message, poll)
     if option[:id] == option_id
       # TODO: users should not be able to vote twice for the same option
       option[:votees] << message.from
+      yield option
       option
     else
       option
@@ -60,6 +61,7 @@ end
 
 def finish_poll(bot, chat_id, poll)
   poll[:state] = :voting
+  poll[:options] = [] if !poll.has_key?(:options)
 
   bot.api.send_message(chat_id: chat_id, text: 'Alright.')
 end
@@ -77,48 +79,48 @@ Telegram::Bot::Client.run(token) do |bot|
       end
 
       if message.data.start_with?('option')
-        poll[:options] = add_votee(message, poll)
-
-        send_statistics(bot, chat_id, poll)
+        poll[:options] = add_votee(message, poll) do |option|
+          bot.api.send_message(chat_id: chat_id, text: "#{message.from.first_name} voted for *#{option[:text]}*.", parse_mode: :markdown)
+        end
       elsif message.data == 'finish'
         finish_poll(bot, chat_id, poll)
         show_poll(bot, chat_id, poll)
       end
     when Telegram::Bot::Types::Message
+      case message.text
+        when /\/newpoll.*/
+          title = message.text.gsub(/\/newpoll\s*/, '')
+          polls[message.chat.id] = { state: :poll_title, title: title }
+
+          bot.api.send_message(chat_id: message.chat.id, text: "I created a poll with the title *#{title}*. What's your first option? You need at least two.", parse_mode: :markdown)
+      end
+
       if poll.nil?
         case message.text
         when '/start'
           bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
         when '/stop'
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-        when '/newpoll'
-          bot.api.send_message(chat_id: message.chat.id, text: "Nice. What's the title of your poll?")
-
-          polls[message.chat.id] = { state: :poll_title }
         end
       else
         case message.text
         when '/finish'
           finish_poll(bot, chat_id, poll)
-
           show_poll(bot, chat_id, poll)
-        else
-          case poll[:state]
-          when :poll_title
-            poll[:title] = message.text
+        when /\/option.*/
+          option = message.text.gsub(/\/option\s*/, '')
 
-            bot.api.send_message(chat_id: message.chat.id, text: "I created a poll with the title \"#{poll[:title]}\". What's your first option? You need at least two.")
+          poll[:options] = [] if poll[:options].nil?
+          poll[:options] << { id: SecureRandom.hex, text: option, votees: [] }
 
-            poll[:state] = :poll_options
-          when :poll_options
-            poll[:options] = [] if poll[:options].nil?
-            poll[:options] << { id: SecureRandom.hex, text: message.text, votees: [] }
+          keyboard = [Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Finish poll', callback_data: 'finish')]
+          markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: keyboard)
 
-            keyboard = [Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Finish poll', callback_data: 'finish')]
-            markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: keyboard)
-
-            bot.api.send_message(chat_id: message.chat.id, text: "Added option. What's your next option? Use `/finish` to end setting up the poll.", reply_markup: markup)
-          end
+          bot.api.send_message(chat_id: message.chat.id, text: "Added option. What's your next option? Use `/finish` to end setting up the poll.", reply_markup: markup)
+        when '/showpoll'
+          show_poll(bot, chat_id, poll)
+        when '/showstats'
+          send_statistics(bot, chat_id, poll)
         end
       end
     end
